f710e85013e2d4c8374f8807461f8cd35f933d94
Andrew Scott <andrewscott02@icloud.com>
2024-04-02 09:06:29 +0100

    Video Autoplay

--- js/app.js
+++ js/app.js
@@ -401,6 +401,17 @@ function CheckSelectorOpacities(selector)
 
 //#endregion
 
+//#region Autoplay Banner Video
+
+$(document).ready(()=>{
+    document.querySelector('video').autoplay = true;
+    document.querySelector('video').play();
+});
+
+
+
+//#endregion
+
 //#region Helper Functions
 
 /** Random Range Function
@@ -429,56 +440,5 @@ function InViewport(element, bounds)
 
     return elementBottom > viewportTop && elementTop < viewportBottom;
 };
-
-//#endregion
-
-
-
-
-
-
-
-//#region Not Implemented
-
-//#region Scale Videos
-
-//$("iframe").fitVids();
-
-// $("iframe").each((item)=>{
-//     $(item).width = $(item).contentWindow.document.body.scrollWidth;
-//     $(item).height = $(item).contentWindow.document.body.scrollHeight;
-// });
-
-//#endregion
-
-//#region Mouse Hover On Cards
-
-// $(".project").on("mouseleave", (event)=>{
-//     $(event.target).removeClass("show-vid");
-//     //ResizeVidBorders();
-// })
-
-// ResizeVidBorders();
-
-// function ResizeVidBorders()
-// {
-//     alert("Resizing videos");
-
-//     $(".project iframe").each((item)=>{
-//         alert("Resizing " + item + " video");
-//         imgHeight = $(item).prev().outerHeight();
-//         vidHeight = $(item).outerHeight();
-
-//         heightDiff = imgHeight - vidHeight;
-
-//         // vidTopPadding = $(item).css("paddingTop") + heightDiff/2;
-//         // vidBottomPadding = $(item).css("paddingBottom") + heightDiff/2;
-
-//         $(item).css("paddingTop", heightDiff/2)
-//         $(item).css("paddingBottom", heightDiff/2)
-//     })
-// }
-
-//#endregion
 
 //#endregion
