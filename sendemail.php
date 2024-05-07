84b95355dfea849294114e9d9212f6d7195ca802
Andrew Scott <andrewscott02@icloud.com>
2024-05-07 09:14:34 +0100

    Contact Form -Now sends me an email

--- sendemail.php
+++ sendemail.php
@@ -3,6 +3,11 @@ session_start();
 
 include_once("includes/connection.php");
 
+include_once("vendor/autoload.php");
+use PHPMailer\PHPMailer\PHPMailer;
+
+$env = parse_ini_file('.env');
+
 $canSubmit = true;
 
 #region Get Inputs
@@ -23,25 +28,25 @@ $user_email = "";
 if (isset($_POST["user_email"]))
 {
     $user_email = filter_input(INPUT_POST, "user_email", FILTER_SANITIZE_STRING);
-    $user_email .= " via Andrew Scott's Portfolio";
+    // $user_email .= " via Andrew Scott's Portfolio";
 }
 else
 {
     $canSubmit = false;
 }
 
-$user_subject = "This message was sent from your portfolio: \n";
+$user_subject = "";
 
 if (isset($_POST["user_subject"]))
 {
     $user_subject .= filter_input(INPUT_POST, "user_subject", FILTER_SANITIZE_STRING);
 }
 
-$user_message = "";
+$user_message = "The following message was sent by: " . $user_email . "\n \n \n \n";
 
 if (isset($_POST["user_message"]))
 {
-    $user_message = filter_input(INPUT_POST, "user_message", FILTER_SANITIZE_STRING);
+    $user_message .= filter_input(INPUT_POST, "user_message", FILTER_SANITIZE_STRING);
     // $user_message = $_POST["user_message"];
 }
 else
@@ -51,17 +56,44 @@ else
 
 #endregion
 
-function SendEmail($user_name, $user_email, $user_subject, $user_message)
+function SendEmail($user_name, $user_email, $user_message, $user_subject = "From my Portfolio")
 {
-    //Send email here
-    echo "sending email from $user_name with email $user_email";
-    echo "\n";
-    echo "Subject: $user_subject";
-    echo "\n";
-    echo "Message: $user_message";
+    global $env;
+
+    $mail = new PHPMailer(true);
+
+    try
+    {
+        //Server settings
+        // $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+        $mail->isSMTP();
+        $mail->Host = $env["MT_HOST"];
+        $mail->SMTPAuth = true;
+        $mail->Username = $env["MT_USERNAME"];
+        $mail->Password = $env["MT_PASSWORD"];
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        $mail->Port = $env["MT_PORT"];
+
+        //Recipients
+        $mail->setFrom("test@demomailtrap.com", $user_name . " via My Portfolio");
+        $mail->addAddress($env["RECIPIENT"], 'Andrew Scott');
+        $mail->addReplyTo($user_email, $user_name);
+
+        //Content
+        $mail->isHTML(false);
+        $mail->Subject = $user_subject;
+        $mail->Body    = $user_message;
+
+        $mail->send();
+        echo 'Message has been sent';
+    }
+    catch (Exception $e)
+    {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
 }
 
-SendEmail($user_name, $user_email, $user_subject, $user_message);
+SendEmail($user_name, $user_email, $user_message, $user_subject);
 
-// header("Location: index.php");
-// exit();
+header("Location: index.php");
+exit();
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => 'ddac991d2fc0753f66f37d1052c7f495188f8928',
+        'reference' => '32539d9468af3a8d16b8dfd49de63bd56eaa7a3e',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => 'ddac991d2fc0753f66f37d1052c7f495188f8928',
+            'reference' => '32539d9468af3a8d16b8dfd49de63bd56eaa7a3e',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
